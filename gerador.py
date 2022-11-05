89306b3f08b48d851edf47096fcd6ac58f2a8099
joaofbr <95386537+joaofbr@users.noreply.github.com>
2022-11-05 16:48:38 -0300

    attQr

--- gerador.py
+++ gerador.py
@@ -1,13 +1,16 @@
 import qrcode
 
-for i in range(0, 5):
+cont = int(input('quantos qrcodes serão gerados: '))
+cor = input('digite a cor que deseja para o preenchimento: ')
+pCor = "'"+cor+"'"
+for i in range(0, cont):
     qr = input('link para ser convertido: ')
     qrc = qrcode.QRCode(
         version=None,
-        error_correction=qrcode.ERROR_CORRECT_H,
+        error_correction=qrcode.ERROR_CORRECT_H
     )
     qrc.add_data(qr)
-    img = qrc.make_image(fill_color='#3B8435',
+    img = qrc.make_image(fill_color=cor,
                          back_color='#FFFFFF')
     type(img)
     img.save(f'{i}.png')
